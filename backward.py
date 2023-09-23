c80edec205c22acc3f072db258c48583c878ca0c
Bhardwaj Bhaskar <bhaskar0120@gmail.com>
2023-09-24 03:06:42 +0530

    Pass multiplicative and grad test

--- backward.py
+++ backward.py
@@ -13,10 +13,15 @@ class Node:
     def backward(self,factor=1):
         self.grad = factor
         if self.functional:
-            self.left_child.backward(factor=self.left_derivative())
-            self.right_child.backward(factor=self.right_derivative())
+            self.left_child.backward(factor=factor*self.left_derivative())
+            self.right_child.backward(factor=factor*self.right_derivative())
+
     def __add__(self,b):
         return Additive(value=self.value+b.value,functional=True,left_child=self,right_child=b)
+    def __sub__(self,b):
+        return Subtractive(value=self.value-b.value,functional=True,left_child=self,right_child=b)
+    def __mul__(self,b):
+        return Multiplicative(value=self.value*b.value,functional=True,left_child=self,right_child=b)
     def __str__(self):
         return "(%s %s %s)"%(self.left_child.__str__(), self.symbol, self.right_child.__str__())
 
@@ -26,9 +31,47 @@ class Additive(Node):
         Node.__init__(self,value,functional,left_child,right_child)
         self.symbol = '+'
     def left_derivative(self):
-        return 1
+        return 1.0
     def right_derivative(self):
-        return 1
+        return 1.0
+
+class Subtractive(Node):
+    def __init__(self,value=float(),functional=False,left_child=None,right_child=None):
+        Node.__init__(self,value,functional,left_child,right_child)
+        self.symbol = '-'
+    def left_derivative(self):
+        return 1.0
+    def right_derivative(self):
+        return -1.0
+
+class Multiplicative(Node):
+    def __init__(self,value=float(),functional=False,left_child=None,right_child=None):
+        Node.__init__(self,value,functional,left_child,right_child)
+        self.symbol = '*'
+    def left_derivative(self):
+        return self.right_child.value
+    def right_derivative(self):
+        return self.left_child.value
+
+# TODO
+# class Divisive(Node):
+#     def __init__(self,value=float(),functional=False,left_child=None,right_child=None):
+#         Node.__init__(self,value,functional,left_child,right_child)
+#         self.symbol = '/'
+#     def left_derivative(self):
+#         return 1
+#     def right_derivative(self):
+#         return 1
+
+#TODO
+# class Exponential(Node):
+#     def __init__(self,value=float(),functional=False,left_child=None,right_child=None):
+#         Node.__init__(self,value,functional,left_child,right_child)
+#         self.symbol = '**'
+#     def left_derivative(self):
+#         return 1
+#     def right_derivative(self):
+#         return 1
 
 class Number(Node):
     def __init__(self,value):
