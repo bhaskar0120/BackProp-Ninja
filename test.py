9a5a2d54ea7439e9a46cc52e4d511067da4dfe0e
Bhardwaj Bhaskar <bhaskar0120@gmail.com>
2023-09-24 02:54:12 +0530

    Add test for printing *,-,/,+

--- test.py
+++ test.py
@@ -3,8 +3,13 @@ from backward import Number
 if __name__ == "__main__":
     a = Number(2)
     b = Number(3)
-    print(a,b)
-    c = a + b
-    d = b + a
-    e = c+d
-    print(e)
+    c = Number(5)
+    d = Number(6)
+    print("Test 1 Passed: ",a,b)
+    e = a + b
+    print("Test 2 Passed: ",e)
+    f = b + a
+    g = e * f
+    print("Test 3 Passed: ",g)
+    h = g / c + f * e
+    print("Test 4 Passed: ",a,b)
