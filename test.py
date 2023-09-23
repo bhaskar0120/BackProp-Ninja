0733183d459223921cc5bc99a5ac9eb635befdef
Bhardwaj Bhaskar <bhaskar0120@gmail.com>
2023-09-24 03:18:17 +0530

    Add better testing setup

--- test.py
+++ test.py
@@ -6,26 +6,39 @@ if __name__ == "__main__":
         b = Number(3)
         c = Number(5)
         d = Number(6)
+        i = Number(10)
         print("Test 1 Passed: ",a,b)
-    except(Exception):
-        print("Test 1 Failed")
+    except Exception as E:
+        print("Test 1 Failed",E)
 
     try:
         e = a + b
         print("Test 2 Passed: ",e)
-    except(Exception):
-        print("Test 2 Failed")
+    except Exception as E:
+        print("Test 2 Failed",E)
 
     try:
-        f = b + a
+        f = c + d
         g = e * f
         print("Test 3 Passed: ",g)
-    except(Exception):
-        print("Test 3 Failed")
+    except Exception as E:
+        print("Test 3 Failed",E)
 
     try:
-        h = g / c + f * e
+        h = g / i - f * e
         print("Test 4 Passed: ",a,b)
-    except(Exception):
-        print("Test 4 Failed")
+    except Exception as E:
+        print("Test 4 Failed",E)
+
+    try:
+        e.backward()
+        print("Test 5 Passed: ",a.grad,b.grad)
+    except Exception as E:
+        print("Test 5 Failed",E)
+
+    try:
+        h.backward()
+        print("Test 6 Passed: ",a.grad,b.grad,c.grad,d.grad,e.grad,f.grad,g.grad,i.grad)
+    except Exception as E:
+        print("Test 6 Failed",E)
 
