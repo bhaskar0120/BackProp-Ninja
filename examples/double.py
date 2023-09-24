e3bb23a736c5321364c419af54ec6470ed74b74a
Bhardwaj Bhaskar <bhaskar0120@gmail.com>
2023-09-24 18:42:04 +0530

    Refined double.py

--- examples/double.py
+++ examples/double.py
@@ -2,6 +2,8 @@ def main():
     from backward import Number
     from random import random, seed
     seed(1203)
+
+
     train_x = [Number(2),Number(3),Number(6),Number(7)]
     train_y = [Number(4),Number(6),Number(12),Number(14)]
 
@@ -10,17 +12,18 @@ def main():
     m = Number(random())
     b = Number(random())
 
-    # for i in E = E + (train_y - m*train_x+b)**2
+    # for i in E = E + (train_y - m*train_x-b)**2
 
     E = Number(0)
-    for i in range(4):
-        E = E + (train_y[i] - m*train_x[i]+b)**Number(2)
+    for i in range(len(train_x)):
+        E = E + (train_y[i] - m*train_x[i] + b)**Number(2)
+
     for i in range(1000):
         E.calculate()
         print(E.value)
         E.backward()
-        m.value -= 0.0001*m.grad
-        b.value -= 0.0001*b.grad
+        m.value -= 0.001*m.grad
+        b.value -= 0.001*b.grad
     print(m,b)
 
 
